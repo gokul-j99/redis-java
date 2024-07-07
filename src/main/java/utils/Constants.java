1fcbef11c8fe281ef9cec8fe86e31f1e810320de
Gokul Jayavel <jayavel.g@northeastern.edu>
2024-07-07 01:03:17 -0400

    added handshake 2

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String SYNTAX_ERROR = "-ERR syntax error\r\n";
     public static final String WRONG_TYPE_RESPONSE = "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
     public static final String NON_INT_ERROR = "-ERR value is not an integer or out of range\r\n";
-    public static final String EMPTY_ARRAY_RESPONSE = "+(empty array)\r\n";
+    public static final String EMPTY_ARRAY_RESPONSE = "+( empty array)\r\n";
     public static final String OUT_OF_RANGE_RESPONSE = "+ERR index out of range";
     public static final String FLOAT_ERROR_MESSAGE = "-ERR value is not a valid float\r\n";
     public static final String EXEC_WITHOUT_MULTI = "-ERR EXEC without MULTI\r\n";
