c50876ae369bd29fa93827ff7fd236ea1bad3da1
Gokul Jayavel <jayavel.g@northeastern.edu>
2024-07-07 00:06:33 -0400

    added handshake -1

--- src/main/java/commands/GetCommand.java
+++ src/main/java/commands/GetCommand.java
@@ -8,17 +8,21 @@ import java.util.List;
 public class GetCommand extends RedisCommand {
     @Override
     public String execute(AsyncRequestHandler handler, List<String> command) {
-        if (handler.expiration.containsKey(command.get(1)) && handler.expiration.get(command.get(1)) < System.currentTimeMillis() / 1000) {
-            handler.memory.remove(command.get(1));
-            handler.expiration.remove(command.get(1));
+        String key = command.get(1);
+        Long expiration = handler.expiration.get(key);
+
+        // Check if the key exists and if it has not expired
+        if (expiration != null && expiration < System.currentTimeMillis() / 1000) {
+            handler.memory.remove(key);
+            handler.expiration.remove(key);
             return "$-1\r\n";
+        }
+
+        String value = handler.memory.get(key);
+        if (value != null) {
+            return EncodingUtils.createRedisResponse(value);
         } else {
-            String value = handler.memory.get(command.get(1));
-            if (value != null) {
-                return EncodingUtils.createRedisResponse(value);
-            } else {
-                return "$-1\r\n";
-            }
+            return "$-1\r\n";
         }
     }
 }
