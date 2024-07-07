a1e8fa809abf14158b349f6784682dbe4e0773d9
Gokul Jayavel <jayavel.g@northeastern.edu>
2024-07-07 01:20:33 -0400

    Commnd procces

--- src/main/java/utils/AsyncServer.java
+++ src/main/java/utils/AsyncServer.java
@@ -1,14 +1,12 @@
 package utils;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.net.*;
 import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.logging.*;
-import java.util.zip.GZIPInputStream;
 
 public class AsyncServer {
     private final String host;
@@ -39,7 +37,6 @@ public class AsyncServer {
         this.config.put("dir", dir);
         this.config.put("dbfilename", dbfilename);
 
-        Logger.getLogger(AsyncServer.class.getName()).info("Master server running on port: " + port);
         if (!dir.isEmpty() && !dbfilename.isEmpty()) {
             Path filePath = Paths.get(dir, dbfilename);
             parseRedisFile(filePath);
@@ -50,7 +47,6 @@ public class AsyncServer {
         AsyncServer instance = new AsyncServer(host, port, replicaServer, replicaPort, dir, dbfilename);
         instance.start();
         if (replicaServer != null && replicaPort > 0) {
-            Logger.getLogger(AsyncServer.class.getName()).info("Connecting to master at " + replicaServer + ":" + replicaPort);
             instance.connectToReplicaServer(replicaServer, replicaPort);
         }
         return instance;
@@ -78,7 +74,7 @@ public class AsyncServer {
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
 
             String response = sendPing(reader, writer);
-            if (!"+PONG\r\n".equals(response)) {
+            if (!"+PONG".equals(response)) {
                 throw new IOException("Failed to receive PONG from replica server. Received: " + response);
             }
 
@@ -122,7 +118,6 @@ public class AsyncServer {
         return reader.readLine();
     }
 
-
     private void handleReplicaCommunication(BufferedReader reader, BufferedWriter writer) throws IOException {
         while (true) {
             String response = reader.readLine();
@@ -154,60 +149,9 @@ public class AsyncServer {
     }
 
     private Map<String, String> parseRedisFile(Path filePath) throws IOException {
-        Map<String, String> hashMap = new HashMap<>();
-        Map<String, Long> expirationMap = new HashMap<>();
-        try (DataInputStream dis = new DataInputStream(new GZIPInputStream(new FileInputStream(filePath.toFile())))) {
-            byte[] buffer = new byte[5];
-            dis.readFully(buffer); // Read "REDIS" magic string
-            dis.readInt(); // Read RDB version
-
-            while (dis.available() > 0) {
-                int dataType = dis.readUnsignedByte();
-                switch (dataType) {
-                    case 0x00: // String
-                        String key = readString(dis);
-                        String value = readString(dis);
-                        hashMap.put(key, value);
-                        break;
-                    case 0xFD: // Expired key (seconds)
-                        long expireTimeSeconds = dis.readInt();
-                        dataType = dis.readUnsignedByte();
-                        key = readString(dis);
-                        value = readString(dis);
-                        hashMap.put(key, value);
-                        expirationMap.put(key, expireTimeSeconds * 1000);
-                        break;
-                    case 0xFC: // Expired key (milliseconds)
-                        long expireTimeMilliseconds = dis.readLong();
-                        dataType = dis.readUnsignedByte();
-                        key = readString(dis);
-                        value = readString(dis);
-                        hashMap.put(key, value);
-                        expirationMap.put(key, expireTimeMilliseconds);
-                        break;
-                    case 0xFE: // Select DB
-                        int dbIndex = dis.readUnsignedByte();
-                        break;
-                    case 0xFF: // End of file
-                        break;
-                    default:
-                        throw new IOException("Unsupported RDB data type: " + dataType);
-                }
-            }
-        } catch (IOException e) {
-            Logger.getLogger(AsyncServer.class.getName()).log(Level.SEVERE, "Error parsing RDB file", e);
-            throw e;
-        }
-        this.memory.putAll(hashMap);
-        this.expiration.putAll(expirationMap);
-        return hashMap;
-    }
-
-    private String readString(DataInputStream dis) throws IOException {
-        int length = dis.readInt();
-        byte[] buffer = new byte[length];
-        dis.readFully(buffer);
-        return new String(buffer, StandardCharsets.UTF_8);
+        // Implement the parse_redis_file method to read the Redis RDB file and populate the memory and expiration maps
+        // This is a placeholder method
+        return new HashMap<>();
     }
 
     public Map<String, String> getMemory() {
@@ -226,6 +170,14 @@ public class AsyncServer {
         return replicaPort;
     }
 
+    public static void main(String[] args) {
+        try {
+            AsyncServer.create("127.0.0.1", 6379, null, 0, "", "");
+        } catch (IOException e) {
+            Logger.getLogger(AsyncServer.class.getName()).log(Level.SEVERE, "Failed to start server", e);
+        }
+    }
+
     public List<BufferedWriter> getWriters() {
         return writers;
     }
@@ -233,7 +185,6 @@ public class AsyncServer {
     public void incrementNumAcks() {
         numacks++;
     }
-
     public Map<String, Map<Integer, Map<Integer, List<String>>>> getStreamstore() {
         return streamstore;
     }
